362cb3082749a9e3379b2506cf2a79f0daeddb2e
Camilo <Camilo@Camilo-PC>
2015-03-22 17:14:17 -0500

    diskitos

--- templates/iniciarsesion.tpl
+++ templates/iniciarsesion.tpl
@@ -3,32 +3,28 @@
         <title>Incio Sesion</title>
         <link rel="stylesheet"  href="./css/bootstrap.min.css">
         <link rel="stylesheet"  href="./css/diskitos.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     </head>
     <body>
-        <div class="container">
-        <div class="row arriba ">
-        <div class="col-sm-4 col-sm-offset-4">
-        <form class="form-horizontal">
-            <div class="form-group">
-              <label for="inputid" class="col-sm-2 control-label p">Identificación</label>
-              <div class="col-sm-10 p">
-                <input type="number" class="form-control" id="inputid" placeholder="Identificación">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="inputcontrasena" class="col-sm-2 control-label p">Contraseña</label>
-              <div class="col-sm-10 p">
-                <input type="password" class="form-control" id="inputcontrasena" placeholder="Contraseña">
-              </div>
-            </div>
-            <div class="form-group ">
-              <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default ">Ingresar</button>
-              </div>
-            </div>
-            </div>
-        </div>
-        </div>
-      </form>
+     <div class="container">
+     <div class="row">
+         <div class="col-md-offset-5 col-md-3 ">
+             <div class="form-login ">
+             <h4>Bienvenido a Diskitos S.A</h4>
+             <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Identificación" />
+             </br>
+             <input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="Contraseña" />
+             </br>
+             <div class="wrapper">
+                 <button type="submit" class="btn btn-primary btn-sm">
+                     <span class="glyphicon glyphicon-log-in "></span> Ingresar
+                 </button>
+             
+             </div>
+             </div>
+
+         </div>
+     </div>
+ </div>
     </body>
 </html>
--- templates_c/e45c917e989251f8842feff697a64e4579c842c0.file.iniciarsesion.tpl.php
+++ templates_c/e45c917e989251f8842feff697a64e4579c842c0.file.iniciarsesion.tpl.php
@@ -1,17 +1,17 @@
-<?php /* Smarty version Smarty-3.0.9, created on 2015-03-22 21:27:58
+<?php /* Smarty version Smarty-3.0.9, created on 2015-03-22 22:12:35
          compiled from "C:/wamp/www/diskitos/templates\iniciarsesion.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:22205550f33de374991-17296863%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:10098550f3e53cb68c1-61505442%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     'e45c917e989251f8842feff697a64e4579c842c0' => 
     array (
       0 => 'C:/wamp/www/diskitos/templates\\iniciarsesion.tpl',
-      1 => 1427059670,
+      1 => 1427062353,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '22205550f33de374991-17296863',
+  'nocache_hash' => '10098550f3e53cb68c1-61505442',
   'function' => 
   array (
   ),
@@ -22,32 +22,28 @@ $_smarty_tpl->decodeProperties(array (
         <title>Incio Sesion</title>
         <link rel="stylesheet"  href="./css/bootstrap.min.css">
         <link rel="stylesheet"  href="./css/diskitos.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     </head>
     <body>
-        <div class="container">
-        <div class="row arriba ">
-        <div class="col-sm-4 col-sm-offset-4">
-        <form class="form-horizontal">
-            <div class="form-group">
-              <label for="inputid" class="col-sm-2 control-label p">Identificación</label>
-              <div class="col-sm-10 p">
-                <input type="number" class="form-control" id="inputid" placeholder="Identificación">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="inputcontrasena" class="col-sm-2 control-label p">Contraseña</label>
-              <div class="col-sm-10 p">
-                <input type="password" class="form-control" id="inputcontrasena" placeholder="Contraseña">
-              </div>
-            </div>
-            <div class="form-group ">
-              <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default ">Ingresar</button>
-              </div>
-            </div>
-            </div>
-        </div>
-        </div>
-      </form>
+     <div class="container">
+     <div class="row">
+         <div class="col-md-offset-5 col-md-3 ">
+             <div class="form-login ">
+             <h4>Bienvenido a Diskitos S.A</h4>
+             <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Identificación" />
+             </br>
+             <input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="Contraseña" />
+             </br>
+             <div class="wrapper">
+                 <button type="submit" class="btn btn-primary btn-sm">
+                     <span class="glyphicon glyphicon-log-in "></span> Ingresar
+                 </button>
+             
+             </div>
+             </div>
+
+         </div>
+     </div>
+ </div>
     </body>
 </html>
