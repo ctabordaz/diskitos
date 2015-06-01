7d406671a97436778c7a86b3dfe03d206f404ac0
AyrtonRA <AyrtonRA@AyrtonRA-PC>
2015-06-01 12:34:45 -0500

    registrar encargo

--- templates/registrarencargo.tpl
+++ templates/registrarencargo.tpl
@@ -30,7 +30,7 @@
                 
                 <div class="row">
                     <div class="col-md-2">
-                        <h4>Cliente:</h4>
+                        <label>Cliente:</label>
                     </div>
                     
                     <div class="col-md-4">
@@ -57,47 +57,7 @@
                                 <input type="text" name="titulo" id="titulo" required />
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Intérprete:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="text" name="interprete" id="interprete" required/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Nro Catálogo:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="number" name="nro_catalogo" min="0" id="catalogo" required/>
-                                <input  type="hidden" name="album"/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>País de Origen:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="text" name="pais_origen" id="paisorigen" required/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Disquera:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="text" name="disquera" id="disquera" required/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Año de publicación:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="number" min="0" max="2020" name="ano_publicacion" id="agepublicacion" required/>
-                            </div>
-                        </div>
+                        
                         <div class="row">
                             <div class="col-md-4">
                                 <p>Género:</p>
@@ -106,64 +66,29 @@
                                 <input  type="text" name="genero" id="genero" required/>
                             </div>
                         </div>
+
                         <div class="row">
-                            <p id="caratula1">Carátula:</p>
-                        </div>
-                        <div class="row required">
-                            <input id="caratula2" type="file" name="caratula">
+                            <h4 id="caratula1">Formato</h4>
+                            <div class="col-md-5">
+                                <select class="form-control" name="formato" type="text" id="formatot" placeholder="Formato">
+                                    <option value ="cd">CD</option>
+                                    <option value ="dvd">DVD</option>
+                                    <option value ="lp">LP</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-7">
-
-                        <h4>Canciones</h4>
-                        <input type="hidden" id="nca" name="nca" value="1">
-                        <div id="cancion">
-                            <div class="sings">
-                                <div class="row">
-                                    <div class="col-md-2">
-                                        <p>Nombre: </p>
-                                    </div>
-                                    <div class="col-md-10 required">
-                                        <input  type="text" name="ncancion1" id="ncancion1" required/>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                     <div class="col-md-2">
-                                        <p>Compositor: </p>
-                                    </div>
-                                    <div class="col-md-10 required">
-                                        <input  type="text" name="ccancion1" id="ccancion1" required/>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                     <div class=" col-md-1 ">
-                                        <p>Duración: </p>
-                                    </div>
-                                    <div class="col-md-offset-1 col-md-3 required2">
-                                        <input  type="number" class="duracion" min="0" max="10" name="duraciona1" id="duraciona1" required/>
-                                    </div>
-
-                                    <div class="col-md-5 required">
-                                        <input  type="number" class="duracion" min="0" max="59" name="duracionb1" id="duracionb1" required/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+                    <div class="col-md-5">
                         <div class="row">
-                            <div class="col-md-1 col-md-2">
-                                <p>
-                                    <button type="button" class="btn btn-default btn-sm" onclick="ms.agregar()">
-                                        <span class="glyphicon glyphicon-plus-sign"></span>Agregar
-                                    </button>
-                                </p>
+                            <br>
+                            <br>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <p>Intérprete:</p>
                             </div>
-                            <div class="col-md-2 col-md-offset-2">
-                                <p>
-                                    <button type="button" class="btn btn-default btn-sm" onclick="ms.eliminar()">
-                                        <span class="glyphicon glyphicon-remove-sign"></span>Remover
-                                    </button>
-                                </p>
+                            <div class="col-md-8 required">
+                                <input  type="text" name="interprete" id="interprete" required/>
                             </div>
                         </div>
                     </div>
--- templates_c/708b26aaeb71a08857e14291becc68eff3fa0966.file.registrarencargo.tpl.php
+++ templates_c/708b26aaeb71a08857e14291becc68eff3fa0966.file.registrarencargo.tpl.php
@@ -1,17 +1,17 @@
-<?php /* Smarty version Smarty-3.0.9, created on 2015-06-01 18:57:17
+<?php /* Smarty version Smarty-3.0.9, created on 2015-06-01 19:32:40
          compiled from "C:/wamp/www/diskitos/templates\registrarencargo.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:1461556c8eed1ec183-24911797%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:11867556c9738524126-38891563%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     '708b26aaeb71a08857e14291becc68eff3fa0966' => 
     array (
       0 => 'C:/wamp/www/diskitos/templates\\registrarencargo.tpl',
-      1 => 1433177834,
+      1 => 1433179934,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '1461556c8eed1ec183-24911797',
+  'nocache_hash' => '11867556c9738524126-38891563',
   'function' => 
   array (
   ),
@@ -51,7 +51,7 @@ registrarencargo.php?option=registrar" method="post" enctype="multipart/form-dat
                 
                 <div class="row">
                     <div class="col-md-2">
-                        <h4>Cliente:</h4>
+                        <label>Cliente:</label>
                     </div>
                     
                     <div class="col-md-4">
@@ -85,47 +85,7 @@ if ($_smarty_tpl->_count($_from) > 0){
                                 <input type="text" name="titulo" id="titulo" required />
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Intérprete:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="text" name="interprete" id="interprete" required/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Nro Catálogo:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="number" name="nro_catalogo" min="0" id="catalogo" required/>
-                                <input  type="hidden" name="album"/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>País de Origen:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="text" name="pais_origen" id="paisorigen" required/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Disquera:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="text" name="disquera" id="disquera" required/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Año de publicación:</p>
-                            </div>
-                            <div class="col-md-8 required">
-                                <input  type="number" min="0" max="2020" name="ano_publicacion" id="agepublicacion" required/>
-                            </div>
-                        </div>
+                        
                         <div class="row">
                             <div class="col-md-4">
                                 <p>Género:</p>
@@ -134,64 +94,29 @@ if ($_smarty_tpl->_count($_from) > 0){
                                 <input  type="text" name="genero" id="genero" required/>
                             </div>
                         </div>
+
                         <div class="row">
-                            <p id="caratula1">Carátula:</p>
-                        </div>
-                        <div class="row required">
-                            <input id="caratula2" type="file" name="caratula">
+                            <h4 id="caratula1">Formato</h4>
+                            <div class="col-md-5">
+                                <select class="form-control" name="formato" type="text" id="formatot" placeholder="Formato">
+                                    <option value ="cd">CD</option>
+                                    <option value ="dvd">DVD</option>
+                                    <option value ="lp">LP</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-7">
-
-                        <h4>Canciones</h4>
-                        <input type="hidden" id="nca" name="nca" value="1">
-                        <div id="cancion">
-                            <div class="sings">
-                                <div class="row">
-                                    <div class="col-md-2">
-                                        <p>Nombre: </p>
-                                    </div>
-                                    <div class="col-md-10 required">
-                                        <input  type="text" name="ncancion1" id="ncancion1" required/>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                     <div class="col-md-2">
-                                        <p>Compositor: </p>
-                                    </div>
-                                    <div class="col-md-10 required">
-                                        <input  type="text" name="ccancion1" id="ccancion1" required/>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                     <div class=" col-md-1 ">
-                                        <p>Duración: </p>
-                                    </div>
-                                    <div class="col-md-offset-1 col-md-3 required2">
-                                        <input  type="number" class="duracion" min="0" max="10" name="duraciona1" id="duraciona1" required/>
-                                    </div>
-
-                                    <div class="col-md-5 required">
-                                        <input  type="number" class="duracion" min="0" max="59" name="duracionb1" id="duracionb1" required/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+                    <div class="col-md-5">
                         <div class="row">
-                            <div class="col-md-1 col-md-2">
-                                <p>
-                                    <button type="button" class="btn btn-default btn-sm" onclick="ms.agregar()">
-                                        <span class="glyphicon glyphicon-plus-sign"></span>Agregar
-                                    </button>
-                                </p>
+                            <br>
+                            <br>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <p>Intérprete:</p>
                             </div>
-                            <div class="col-md-2 col-md-offset-2">
-                                <p>
-                                    <button type="button" class="btn btn-default btn-sm" onclick="ms.eliminar()">
-                                        <span class="glyphicon glyphicon-remove-sign"></span>Remover
-                                    </button>
-                                </p>
+                            <div class="col-md-8 required">
+                                <input  type="text" name="interprete" id="interprete" required/>
                             </div>
                         </div>
                     </div>
